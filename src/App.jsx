29d3924e1a0289a3f02a721a056b39fc3a258059
胡志伟5 <huzhiwei5@hikvision.com>
2021-03-09 13:17:33 +0800

    小修改

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
 import React from "react";
-import classnames from "classnames";
-import Styles from "./Category.less";
-import Search from "@components/search/Search";
-import Menu from "../container/Menu";
 
-const CategoryUi = (props) => {
-  const { tabIndex, onCategoryUiClick, type } = props;
-  return (
-    <div className={Styles.CategoryWrap}>
-      <nav>
-        <ul>
-          <li
-            className={tabIndex === 0 ? Styles.active : ""}
-            onClick={onCategoryUiClick(0)}
-          >
-            分类
-            <li
-              className={tabIndex === 1 ? Styles.active : ""}
-              onClick={onCategoryUiClick(1)}
-            >
-              食材
-            </li>
-            <li
-              className={
-                tabIndex === 0
-                  ? Styles.slide
-                  : classnames(Styles.slide, Styles.right)
-              }
-            ></li>
-          </li>
-        </ul>
-      </nav>
-      <Search outerbg="#fff" innerbg="#efefef" hasborder={false}></Search>
-      <Menu type={type}></Menu>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
 
-export default CategoryUi;
+export default App;
