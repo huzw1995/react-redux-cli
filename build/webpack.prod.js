2cee8389487fa347329c27004458dfbbfe142ab9
胡志伟5 <huzhiwei5@hikvision.com>
2021-03-15 13:41:51 +0800

    添加emotion依赖

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -8,7 +8,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   mode: "production", // 生产模式
-  devtool: "cheap-module-source-map",
+  devtool: "source-map",
   output: {
     publicPath: "./",
   },
