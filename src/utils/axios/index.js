a16b8ef6329680c4aff73f3f453980831a444d71
胡志伟5 <huzhiwei5@hikvision.com>
2021-03-15 20:08:21 +0800

    重写axios拦截器逻辑

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 import _Error from "../error";
 
-let cancelList = [];
-
-const CancelToken = axios.CancelToken;
 let token = document.getElementsByName("_csrf")[0].getAttribute("content");
 let header = document
   .getElementsByName("_csrf_header")[0]
   .getAttribute("content");
 axios.defaults.headers.common[header] = token;
+const cancelToken = axios.CancelToken;
+let pending = []; //声明一个数组用于存储每个请求的取消函数和axios标识
+let removePending = (config) => {
+  // console.log(config);
+  for (let i in pending) {
+    if (pending[i].url === axios.defaults.baseURL + config.url) {
+      //在当前请求在数组中存在时执行取消函数
+      pending[i].f(); //执行取消操作
+      //pending.splice(i, 1); 根据具体情况决定是否在这里就把pending去掉
+      console.log(pending[i].url);
+    }
+  }
+};
 //1,创建axios实例
 // 为什么要实现axios的实例来创建请求，而不是使用全局的axios
 // 因为可能一个项目可以有多个域的请求，那么一个全局的axios就无法满足要求了
 // 根据不用的模块创建不同的axios实例
-const instance = axios.create({
+const http = axios.create({
   // 请求时会拼接baseurl
   baseURL: "/",
   // 超时毫秒数
@@ -22,25 +32,29 @@ const instance = axios.create({
 
 // 2,axios的拦截器
 // 2.1,请求拦截的作用
-instance.interceptors.request.use(
+http.interceptors.request.use(
   (config) => {
-    console.log(config);
-    // 1.比如config中的一些信息不符合服务器的要求
-
-    // 2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
-
-    // 3.某些网络请求,必须携带一些特殊的信息
-
-    // 放行
-    return config;
+    removePending(config); //在一个axios发送前执行一下判定操作，在removePending中执行取消操作
+    // console.log(config.url);
+    config.cancelToken = new cancelToken(function executor(c) {
+      //本次axios请求的配置添加cancelToken
+      pending.push({
+        // url: config.url,
+        url: axios.defaults.baseURL + config.url,
+        f: c,
+      });
+      // console.log(axios.defaults.baseURL+config.url);
+      //将本次的url添加到pending中，因此对于某个url第一次发起的请求不会被取消，因为还没有配置取消函数
+    });
+    return Promise.resolve(config);
   },
-  (err) => {
-    // 一般网络异常之类的错误才会进入这里
-    console.log(err);
+  (error) => {
+    return Promise.reject(error);
   }
 );
+
 // 2.2,响应拦截
-instance.interceptors.response.use(
+http.interceptors.response.use(
   (response) => {
     let { status, data } = response;
     if (response) {
@@ -59,46 +73,44 @@ instance.interceptors.response.use(
   },
   (error) => {
     // 返回响应状态码错误时进入
-    if (axios.isCancel(error)) {
-    } else {
-      let response = error.response;
-      if (response) {
-        let status = response.status;
-        if (status === 403) {
-          return Promise.reject(new _Error("您没有权限进行此项操作"));
-        } else if (status === 404 || status === 502 || status === 504) {
-          return Promise.reject(new _Error("网络异常"));
-        }
-        if ("data" in response) {
-          return Promise.reject(new _Error(response.data));
-        } else if ("statusText" in response) {
-          return Promise.reject(new _Error(response.statusText));
-        }
-      } else if (e.message === "Network Error") {
+    let response = error.response;
+    if (response) {
+      let status = response.status;
+      if (status === 403) {
+        return Promise.reject(new _Error("您没有权限进行此项操作"));
+      } else if (status === 404 || status === 502 || status === 504) {
         return Promise.reject(new _Error("网络异常"));
       }
+      if ("data" in response) {
+        return Promise.reject(new _Error(response.data));
+      } else if ("statusText" in response) {
+        return Promise.reject(new _Error(response.statusText));
+      }
+    } else if (e.message === "Network Error") {
+      return Promise.reject(new _Error("网络异常"));
     }
-    return Promise.reject(error);
   }
 );
 
+const get = (url, opts) => {
+  opts = opts || {};
+  return http({
+    url,
+    method: "get",
+    data: opts,
+  });
+};
+
+const post = (url, opts) => {
+  opts = opts || {};
+  return http({
+    url,
+    method: "post",
+    data: opts,
+  });
+};
+
 export default {
-  get(url, opts) {
-    opts = opts || {};
-    opts.cancelToken = new CancelToken(function executor(callback) {
-      cancelList.push(callback);
-    });
-    return axios.get(url, opts);
-  },
-  post(url, opts) {
-    opts = opts || {};
-    opts.cancelToken = new CancelToken(function executor(callback) {
-      cancelList.push(callback);
-    });
-    return axios.post(url, opts);
-  },
-  cancel() {
-    cancelList.forEach((item) => item());
-    cancelList = [];
-  },
+  get,
+  post,
 };
